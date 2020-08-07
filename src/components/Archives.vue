06fec81520a3a55df9719453ae51626130fcd12f
Yujun Jiang <yujunjiang@gmail.com>
2020-08-07 15:13:46 -0400

    search bar and artist names

---
+++ src/components/Archives.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <!-- <div class="search-bar">
+      <input type="text" id="search-input" placeholder="Search" />
+    </div> -->
+
+    <div v-bind:key="archive.id" v-for="archive in archives">
+      <h3>{{archive.name}}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Archives",
+  props: ["archives"]
+}
+</script>
+
+<style scoped>
+/* ---- input ---- */
+
+input[type="text"] {
+  font-family: 'Rajdhani', sans-serif;
+  font-weight: 500;
+  font-size: 20px;
+  width: 600px;
+}
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="search-bar">
+      <input type="text" id="search-input" placeholder="Search" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  props: ["search"]
+}
+</script>
+
+<style scoped>
+/* ---- input ---- */
+
+input[type="text"] {
+  font-family: 'Rajdhani', sans-serif;
+  font-weight: 500;
+  font-size: 20px;
+  width: 600px;
+}
+</style>
